e17700a659282a76e8c454ee3df19b326a5e58d0
lesnikovskiy <lesnikovski@gmail.com>
2018-03-21 23:02:33 +0200

    working with sound

---
+++ OutdoorMovieTheater/index.vr.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  Sound,
+} from 'react-vr';
+
+import MainMenu from './Components/Scenes/MainMenu.js';
+
+export default class OutdoorMovieTheater extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('fort-night.jpg')}>
+          <Sound 
+            volume={0.8}
+            loop={true}
+            source={{mp3: asset('background.mp3')}} />
+        </Pano>
+        <MainMenu />
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('OutdoorMovieTheater', () => OutdoorMovieTheater);
