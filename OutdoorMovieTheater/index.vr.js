3363523313a4c0b76aff5e65ef522070d0e01dfa
MATONE\lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2018-03-22 10:45:34 +0200

    working with video

--- OutdoorMovieTheater/index.vr.js
+++ OutdoorMovieTheater/index.vr.js
@@ -15,20 +15,20 @@ export default class OutdoorMovieTheater extends React.Component {
   render() {
     return (
       <View>
-        {/* <Pano source={asset('fort-night.jpg')}>
+        <Pano source={asset('fort-night.jpg')}>
           <Sound 
             volume={0.8}
             loop={true}
             source={{mp3: asset('background.mp3')}} />
-        </Pano> */}
-        <VideoPano source={
+        </Pano>
+        {/* <VideoPano source={
           asset('video-pano-test.mp4', { format: 'mp4', layout: 'SPHERE' })
         }>
           <Sound 
             volume={0.8}
             loop={true}
             source={{mp3: asset('background.mp3')}} />
-        </VideoPano>
+        </VideoPano> */}
       </View>
     );
   }
