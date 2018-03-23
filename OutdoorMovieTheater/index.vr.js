86a3aaed6a4819904344754fcf919114ed31bc0c
MATONE\lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2018-03-23 12:12:28 +0200

    working with video

--- OutdoorMovieTheater/index.vr.js
+++ OutdoorMovieTheater/index.vr.js
@@ -6,30 +6,24 @@ import {
   Text,
   View,
   Sound,
+  VrButton,
   VideoPano
 } from 'react-vr';
 
 import MainMenu from './Components/Scenes/MainMenu.js';
+import MovieTheater from './Components/Scenes/MovieTheater.js';
 
 export default class OutdoorMovieTheater extends React.Component {
   render() {
     return (
       <View>
-        {/* <Pano source={asset('fort-night.jpg')}>
-          <Sound 
-            volume={0.8}
-            loop={true}
-            source={{mp3: asset('background.mp3')}} />
-        </Pano>
-        <MainMenu /> */}
-        <VideoPano source={
-          asset('video-pano-test.mp4', { format: 'mp4', layout: 'SPHERE' })
-        }>
+        <Pano source={asset('fort-night.jpg')}>
           <Sound
             volume={0.8}
             loop={true}
             source={{ mp3: asset('background.mp3') }} />
-        </VideoPano>
+        </Pano>
+        <MovieTheater />
       </View>
     );
   }
